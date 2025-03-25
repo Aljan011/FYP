5d20076ebba09d1cbfafdb1abf169e07fe610140
[aljan kc] <[np03cs4a220104@heraldcollege.edu.np]>
2025-03-25 22:17:55 +0545

    changes injs

---
+++ Gym_Freak/frontend/static/frontend/75.main.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[75],{6075:(e,n,r)=>{r.r(n),r.d(n,{default:()=>a});var s=r(6540),t=r.n(s);class a extends s.Component{constructor(e){super(e)}render(){return t().createElement("p",null,"This is main user dashboard page")}}}}]);
