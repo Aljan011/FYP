57074357b3c6170b9ce21734d178b5f06f3ac806
[aljan kc] <[np03cs4a220104@heraldcollege.edu.np]>
2025-03-29 20:37:34 +0545

    added login interface

--- Gym_Freak/api/serializers.py
+++ Gym_Freak/api/serializers.py
@@ -1,4 +1,4 @@
 from rest_framework import serializers
 
 from .models import User    
-
+from .models import UserProfile, UserWorkout, UserNutrition, UserProgress, UserGoal, UserActivity, UserSleep, UserBodyMeasurement
